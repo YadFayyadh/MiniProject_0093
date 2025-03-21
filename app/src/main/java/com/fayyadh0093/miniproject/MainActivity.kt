319e2cc71c927d29921e624ccd4d4bdce751d91f
Muhammad Fayyadh Raihan <rehanmuhamad213@gmail.com>
2025-03-21 19:54:03 +0700

    Cek Commit

--- app/src/main/java/com/fayyadh0093/miniproject/MainActivity.kt
+++ app/src/main/java/com/fayyadh0093/miniproject/MainActivity.kt
@@ -30,10 +30,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Fayyadh $name!",
         modifier = modifier
     )
 }
