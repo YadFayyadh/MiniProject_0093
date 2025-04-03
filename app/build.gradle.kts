3c7fca404b4ae7c37538533fb82aaaae179ca55e
Muhammad Fayyadh Raihan <rehanmuhamad213@gmail.com>
2025-04-04 04:21:35 +0700

    Menambahkan depedencies di toml dan di build gradle app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.storage)
+
+    implementation(libs.accompanist.permissions)
+    implementation(libs.coil.compose)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
